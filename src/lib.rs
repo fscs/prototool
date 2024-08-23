20cad4ac5c95b6772cba6dc07378cebd6ff24eed
0x5a4 <54070204+0x5a4@users.noreply.github.com>
2024-08-23 15:37:17 +0200

    style: allow needless returns

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,11 @@
 #![deny(clippy::unwrap_used)]
+
 #![warn(clippy::shadow_same)]
 #![warn(clippy::shadow_reuse)]
 #![warn(clippy::shadow_unrelated)]
 #![warn(clippy::nursery)]
+
+#![allow(clippy::needless_return)]
 
 pub mod post;
 pub mod protokoll;
