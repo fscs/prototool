ac4ebb2d964ce631373d9db1df23a43d42f2f449
0x5a4 <bej86nug@hhu.de>
2025-01-31 00:35:44 +0100

    build: maybe fix build on darwin?

--- flake.nix
+++ flake.nix
@@ -45,7 +45,7 @@
           buildInputs =
             [ ]
             ++ lib.optionals pkgs.stdenv.isDarwin [
-              pkgs.libiconv
+              pkgs.frameworks.AppKit
             ];
         };
 
