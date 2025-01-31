cd2ed784ea291e03f8fa53246e367d5014953d58
0x5a4 <bej86nug@hhu.de>
2025-01-31 01:38:57 +0100

    build: allow for cross compilation

--- flake.nix
+++ flake.nix
@@ -6,6 +6,11 @@
 
     crane.url = "github:ipetkov/crane";
 
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
     flake-utils.url = "github:numtide/flake-utils";
   };
 
@@ -14,80 +19,121 @@
       self,
       nixpkgs,
       crane,
+      rust-overlay,
       flake-utils,
       ...
     }:
-    flake-utils.lib.eachSystem
-      [
+    let
+      systems = [
         "aarch64-linux"
         "x86_64-linux"
-      ]
-      (
-        system:
-        let
-          pkgs = nixpkgs.legacyPackages.${system};
+      ];
+    in
+    flake-utils.lib.eachSystem systems (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        inherit (pkgs) lib;
 
-          inherit (pkgs) lib;
+        craneLib = crane.mkLib pkgs;
 
-          craneLib = crane.mkLib pkgs;
+        markdownFilter = path: _type: builtins.match ".*md$" path != null;
+        markdownOrCargo = path: type: (markdownFilter path type) || (craneLib.filterCargoSources path type);
 
-          markdownFilter = path: _type: builtins.match ".*md$" path != null;
-          markdownOrCargo = path: type: (markdownFilter path type) || (craneLib.filterCargoSources path type);
+        src = lib.cleanSourceWith {
+          src = craneLib.path ./.;
+          filter = markdownOrCargo;
+        };
 
-          src = lib.cleanSourceWith {
-            src = craneLib.path ./.;
-            filter = markdownOrCargo;
-          };
+        crossBuildFor =
+          crossSystem:
+          let
+            crossPkgs = import nixpkgs {
+              inherit crossSystem;
+              localSystem = system;
+              overlays = [ (import rust-overlay) ];
+            };
 
-          commonArgs = {
-            inherit src;
-            strictDeps = true;
+            crossCraneLib = (crane.mkLib crossPkgs).overrideToolchain (p: p.rust-bin.stable.latest.default);
 
-            buildInputs = [ ];
-          };
+            systemTripleMap = {
+              "x86_64-linux" = "X86_64_UNKNOWN_LINUX_GNU";
+              "aarch64-linux" = "AARCH64_UNKNOWN_LINUX_GNU";
+            };
 
-          cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+            crossExpr =
+              { stdenv }:
+              crossCraneLib.buildPackage {
+                inherit src;
+                strictDeps = true;
 
-          prototool-crate = craneLib.buildPackage (
-            commonArgs
+                nativeBuildInputs = [ stdenv.cc ];
+
+                "CARGO_TARGET_${systemTripleMap.${crossSystem}}_LINKER" = "${stdenv.cc.targetPrefix}cc";
+                CARGO_BUILD_TARGET = (
+                  lib.toLower (lib.replaceStrings [ "_" ] [ "-" ] systemTripleMap.${crossSystem})
+                );
+                HOST_CC = "${stdenv.cc.nativePrefix}cc";
+                TARGET_CC = "${stdenv.cc.targetPrefix}cc";
+              };
+          in
+          crossPkgs.callPackage crossExpr { };
+
+        localCommonArgs = {
+          inherit src;
+          strictDeps = true;
+
+          buildInputs = [ ];
+        };
+
+        localCargoArtifacts = craneLib.buildDepsOnly localCommonArgs;
+
+        local-prototool-crate = craneLib.buildPackage (
+          localCommonArgs
+          // {
+            cargoArtifacts = localCargoArtifacts;
+
+            meta.mainProgram = "prototool";
+          }
+        );
+      in
+      {
+        checks = {
+          inherit local-prototool-crate;
+
+          my-crate-test = craneLib.cargoTest (
+            localCommonArgs
             // {
-              inherit cargoArtifacts;
-
-              meta.mainProgram = "prototool";
+              cargoArtifacts = localCargoArtifacts;
             }
           );
-        in
-        {
-          checks = {
-            inherit prototool-crate;
+        };
 
-            my-crate-test = craneLib.cargoTest (
-              commonArgs
-              // {
-                inherit cargoArtifacts;
-              }
-            );
+        packages =
+          {
+            default = local-prototool-crate;
+          }
+          // (lib.listToAttrs (
+            map (crossSystem: lib.nameValuePair "cross-${crossSystem}" (crossBuildFor crossSystem)) systems
+          ));
+
+        devShells = {
+          default = craneLib.devShell {
+            checks = self.checks.${system};
+            nativeBuildInputs = with pkgs; [
+              cargo
+              rustc
+              rustfmt
+              cargo-semver-checks
+            ];
           };
 
-          packages.default = prototool-crate;
-
-          devShells = {
-            default = craneLib.devShell {
-              checks = self.checks.${system};
-              nativeBuildInputs = with pkgs; [
-                cargo
-                rustc
-                rustfmt
-                cargo-semver-checks
-              ];
-            };
-
-            attic = pkgs.mkShell {
-              nativeBuildInputs = [
-                pkgs.attic-client
-              ];
-            };
+          attic = pkgs.mkShell {
+            nativeBuildInputs = [
+              pkgs.attic-client
+            ];
           };
-        }
-      );
+        };
+      }
+    );
 }
